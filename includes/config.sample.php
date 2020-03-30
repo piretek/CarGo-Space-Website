dc4ff2fed8aef47bd6613fc729e675291e20cd7d
Piotr Czarnecki <piretek@piretek.pro>
2020-03-30 14:18:24 +0200

    Add config sample

---
+++ includes/config.sample.php
@@ -0,0 +1,9 @@
+<?php
+
+// Database credentials config
+$config['db']['host'] = 'localhost';
+$config['db']['login'] = 'root';
+$config['db']['pass'] = '';
+$config['db']['name'] = 'name';
+
+return $config;
