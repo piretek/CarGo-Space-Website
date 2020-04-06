cb33ecfb66668c4bb7ec8650a3f1582189d9979b
Piotr Czarnecki <piretek@piretek.pro>
2020-04-06 14:11:28 +0200

    Add table styling

--- dashboard.php
+++ dashboard.php
@@ -25,7 +25,7 @@ include './includes/header.php';
                 <h3 class='page--title'></h3>
               </div>
               <div class="column col-50">
-                <p class='nm'>
+                <p class='nm ta-right'>
                   Witaj, <?= $sessionUser['email'] ?>
                   <a class='logout-link' href='logout.php'>
                     <button>Wyloguj się</button>
@@ -35,10 +35,78 @@ include './includes/header.php';
             </div>
             <div class="columns">
               <div id='page-rents' class="page column col-100">
-
+                <table>
+                 <tbody>
+                    <tr>
+                      <th>Klient</th>
+                      <th>Samochód</th>
+                      <th>Czas</th>
+                      <th>Akcje</th>
+                    </tr>
+                    <tr>
+                      <td>Piotr Czarnecki</td>
+                      <td>Audi A4 I LU 0123A</td>
+                      <td>06.04.2020 - 08.04.2020</td>
+                      <td>Zobacz | Edytuj | Usuń</td>
+                    </tr>
+                    <tr>
+                      <td>Piotr Czarnecki</td>
+                      <td>Audi A4 I LU 0123A</td>
+                      <td>06.04.2020 - 08.04.2020</td>
+                      <td>Zobacz | Edytuj | Usuń</td>
+                    </tr>
+                    <tr>
+                      <td>Piotr Czarnecki</td>
+                      <td>Audi A4 I LU 0123A</td>
+                      <td>06.04.2020 - 08.04.2020</td>
+                      <td>Zobacz | Edytuj | Usuń</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
-              <div id='page-fleet' class="page column col-100"></div>
-              <div id='page-users' class="page column col-100"></div>
+              <div id='page-fleet' class="page column col-100">
+                <div class='columns'>
+                  <div class="column col-100">
+                    Dodaj pojazd
+                  </div>
+                  <div class="column col-100">
+                    <table>
+                     <tbody>
+                        <tr>
+                          <th>Samochód</th>
+                          <th>Rejestracja</th>
+                          <th>Wypożyczony</th>
+                          <th>Akcje</th>
+                        </tr>
+                        <tr>
+                          <td>Audi A4 I (B5)</td>
+                          <td>LU 0123A</td>
+                          <td>Tak (06.04.2020 - 08.04.2020)</td>
+                          <td>Zobacz | Edytuj | Usuń</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+              <div id='page-users' class="page column col-100">
+                <table>
+                  <tbody>
+                    <tr>
+                      <th>Email</th>
+                      <th>Imię i nazwisko</th>
+                      <th>Uprawnienia</th>
+                      <th>Akcje</th>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td>LU 0123A</td>
+                      <td></td>
+                      <td>Edytuj | Usuń</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
               <div id='page-account' class="page column col-100"></div>
             </div>
           </div>
