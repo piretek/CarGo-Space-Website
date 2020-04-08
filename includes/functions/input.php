82b6eced6c50528cfc15b047a1bf2d62c550c814
Piotr Czarnecki <piretek@piretek.pro>
2020-04-08 20:18:49 +0200

    Add radio and checkbox input support

--- includes/functions/input.php
+++ includes/functions/input.php
@@ -2,16 +2,37 @@
 
 if (!defined('SECURE_BOOT')) exit;
 
-function input( $id, $label, $value = '', $placeholder = '', $type = 'text', $errorPrefix = null) {
+function input( $id, $label, $value = '', $placeholder = '', $type = 'text', $errorPrefix = null, $addtiotionalAttributes = []) {
   global $_SESSION;
 
-  if ($errorPrefix === null) $errorPrefix = pathinfo(__DIR__.$_SERVER['PHP_SELF'], PATHINFO_FILENAME); ?>
+  $defaultAttributes = [
+    'id' => $id,
+    'type' => $type,
+    'name' => $type == 'checkbox' ? $placeholder.'[]' : $type == 'radio' ? $placeholder : $id,
+    'placeholder' => $placeholder,
+    'value' => $value,
+  ];
 
+  $attributes = array_merge($defaultAttributes, $addtiotionalAttributes);
+
+  $attributesText = '';
+  foreach($attributes as $attribute => $value) {
+    $attributesText .= "{$attribute}='{$value}' ";
+  }
+
+  if ($errorPrefix === null) $errorPrefix = pathinfo(__DIR__.$_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+
+  if ($type == 'checkbox' || $type == 'radio') : ?>
+    <div class='input--container rc'>
+      <input class='input' <?= $attributesText ?>>
+      <label class='input--label' for="<?= $id ?>"><?= $label ?></label>
+    </div>
+    <span class='input--error'><?php $errField = $id; if (isset($_SESSION[$errorPrefix.'-form-error-'.$errField])) { echo $_SESSION[$errorPrefix.'-form-error-'.$errField]; unset($_SESSION[$errorPrefix.'-form-error-'.$errField]); } ?></span>
+  <?php else : ?>
     <div class='input--container'>
       <label class='input--label' for="<?= $id ?>"><?= $label ?></label>
-      <input class='input' id="<?= $id ?>" type="<?= $type ?>" name="<?= $id ?>" placeholder="<?= $placeholder ?>" value='<?= $value ?>'>
+      <input class='input' <?= $attributesText ?>>
       <span class='input--error'><?php $errField = $id; if (isset($_SESSION[$errorPrefix.'-form-error-'.$errField])) { echo $_SESSION[$errorPrefix.'-form-error-'.$errField]; unset($_SESSION[$errorPrefix.'-form-error-'.$errField]); } ?></span>
     </div>
-
-  <?php
+  <?php endif;
 }
