f02af45fe3576ecf26aeaa94eff8eda65364c0e1
Piotr Czarnecki <piretek@piretek.pro>
2020-04-09 12:15:43 +0200

    Add image url to carInfo function

--- includes/functions/carinfo.php
+++ includes/functions/carinfo.php
@@ -3,7 +3,8 @@
 if (!defined('SECURE_BOOT')) exit;
 
 function carinfo($carId) {
-  global $db;
+  global $db, $config;
+
   $query = sprintf("SELECT cars.id, brands.name as brand, models.model as model, types.name as type, year, engine, clutch, fuel, IF(cars.price = 0.00, models.price, cars.price) as price, image, registration FROM (((cars INNER JOIN models ON cars.model = models.id) INNER JOIN types ON types.id = models.type) INNER JOIN brands ON brands.id = models.brand) WHERE cars.id = '%s'",
     $db->real_escape_string($carId)
   );
@@ -13,6 +14,9 @@ function carinfo($carId) {
     return null;
   }
   else {
-    return $cars->fetch_assoc();
+    $car = $cars->fetch_assoc();
+    if (!empty($car['image'])) $car['image'] = $config['site_url'].'/'.$config['car_photo_upload_dir'].'/'.$car['image'];
+
+    return $car;
   }
 }
