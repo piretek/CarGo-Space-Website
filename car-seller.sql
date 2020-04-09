7cf55c8c0a4df8aec175dd52b2cd3abe70337b36
Piotr Czarnecki <piretek@piretek.pro>
2020-04-09 14:49:28 +0200

    Modify actual database scheme

--- car-seller.sql
+++ car-seller.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Czas generowania: 05 Kwi 2020, 19:51
--- Wersja serwera: 10.4.10-MariaDB
--- Wersja PHP: 7.3.12
+-- Host: 127.0.0.1
+-- Czas generowania: 09 Kwi 2020, 14:48
+-- Wersja serwera: 10.4.8-MariaDB
+-- Wersja PHP: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,20 +28,10 @@ SET time_zone = "+00:00";
 -- Struktura tabeli dla tabeli `brands`
 --
 
-DROP TABLE IF EXISTS `brands`;
-CREATE TABLE IF NOT EXISTS `brands` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Zrzut danych tabeli `brands`
---
-
-INSERT INTO `brands` (`id`, `name`) VALUES
-(1, 'Audi'),
-(2, 'BMW');
+CREATE TABLE `brands` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -49,25 +39,17 @@ INSERT INTO `brands` (`id`, `name`) VALUES
 -- Struktura tabeli dla tabeli `cars`
 --
 
-DROP TABLE IF EXISTS `cars`;
-CREATE TABLE IF NOT EXISTS `cars` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `cars` (
+  `id` int(11) NOT NULL,
   `model` int(11) NOT NULL,
   `year` year(4) NOT NULL,
-  `engine` varchar(20) NOT NULL,
-  `clutch` varchar(20) NOT NULL,
-  `registration` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `cars_models` (`model`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Zrzut danych tabeli `cars`
---
-
-INSERT INTO `cars` (`id`, `model`, `year`, `engine`, `clutch`, `registration`) VALUES
-(1, 2, 2000, '1.9 tdi', 'manual', 'LU 1612C'),
-(2, 2, 2001, '2.0 tdi', 'manual', 'LUB 1730K');
+  `engine` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `fuel` varchar(15) CHARACTER SET utf8 NOT NULL,
+  `clutch` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `registration` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `image` varchar(100) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -75,9 +57,8 @@ INSERT INTO `cars` (`id`, `model`, `year`, `engine`, `clutch`, `registration`) V
 -- Struktura tabeli dla tabeli `clients`
 --
 
-DROP TABLE IF EXISTS `clients`;
-CREATE TABLE IF NOT EXISTS `clients` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
   `name` varchar(20) CHARACTER SET utf8 NOT NULL,
   `surname` varchar(25) CHARACTER SET utf8 NOT NULL,
   `city` varchar(25) CHARACTER SET utf8 NOT NULL,
@@ -85,10 +66,8 @@ CREATE TABLE IF NOT EXISTS `clients` (
   `number` varchar(10) CHARACTER SET utf8 NOT NULL,
   `phone` varchar(9) CHARACTER SET utf8 NOT NULL,
   `email` varchar(30) CHARACTER SET utf8 NOT NULL,
-  `pesel` varchar(11) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `pesel` (`pesel`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `pesel` varchar(11) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -96,25 +75,15 @@ CREATE TABLE IF NOT EXISTS `clients` (
 -- Struktura tabeli dla tabeli `models`
 --
 
-DROP TABLE IF EXISTS `models`;
-CREATE TABLE IF NOT EXISTS `models` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `models` (
+  `id` int(11) NOT NULL,
   `brand` int(11) NOT NULL,
   `type` int(11) NOT NULL,
-  `model` varchar(30) NOT NULL,
+  `model` varchar(30) CHARACTER SET utf8 NOT NULL,
   `year_from` year(4) NOT NULL,
   `year_to` year(4) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `models_brands` (`brand`),
-  KEY `models_types` (`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Zrzut danych tabeli `models`
---
-
-INSERT INTO `models` (`id`, `brand`, `type`, `model`, `year_from`, `year_to`) VALUES
-(2, 1, 1, 'A4 I', 1994, 2001);
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -122,18 +91,14 @@ INSERT INTO `models` (`id`, `brand`, `type`, `model`, `year_from`, `year_to`) VA
 -- Struktura tabeli dla tabeli `rents`
 --
 
-DROP TABLE IF EXISTS `rents`;
-CREATE TABLE IF NOT EXISTS `rents` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `rents` (
+  `id` int(11) NOT NULL,
   `client` int(11) NOT NULL,
   `car` int(11) NOT NULL,
   `begin` int(11) NOT NULL,
   `end` int(11) NOT NULL,
-  `email_sent` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `rents_cars` (`car`),
-  KEY `rents_clients` (`client`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `email_sent` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -141,19 +106,10 @@ CREATE TABLE IF NOT EXISTS `rents` (
 -- Struktura tabeli dla tabeli `types`
 --
 
-DROP TABLE IF EXISTS `types`;
-CREATE TABLE IF NOT EXISTS `types` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Zrzut danych tabeli `types`
---
-
-INSERT INTO `types` (`id`, `name`) VALUES
-(1, 'Combi');
+CREATE TABLE `types` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -161,15 +117,111 @@ INSERT INTO `types` (`id`, `name`) VALUES
 -- Struktura tabeli dla tabeli `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(80) NOT NULL,
-  `firstname` varchar(20) NOT NULL,
-  `lastname` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(80) CHARACTER SET utf8 NOT NULL,
+  `firstname` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `lastname` varchar(30) CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cars_models` (`model`);
+
+--
+-- Indeksy dla tabeli `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `pesel` (`pesel`);
+
+--
+-- Indeksy dla tabeli `models`
+--
+ALTER TABLE `models`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `models_brands` (`brand`),
+  ADD KEY `models_types` (`type`);
+
+--
+-- Indeksy dla tabeli `rents`
+--
+ALTER TABLE `rents`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `rents_cars` (`car`),
+  ADD KEY `rents_clients` (`client`);
+
+--
+-- Indeksy dla tabeli `types`
+--
+ALTER TABLE `types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `models`
+--
+ALTER TABLE `models`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `rents`
+--
+ALTER TABLE `rents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `types`
+--
+ALTER TABLE `types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Ograniczenia dla zrzutów tabel
