629dec5fdf244b89ba5a28122f8e532344bbcc41
Piotr Czarnecki <piretek@piretek.pro>
2020-04-16 16:40:11 +0200

    Add prices per car in rent_price function

--- includes/functions/rent_price.php
+++ includes/functions/rent_price.php
@@ -1,11 +1,18 @@
 <?php
 
-function rent_price( $rentId ) {
+function rent_price( $id, $isCar = false, $from = null, $to = null ) {
   global $db;
 
-  $query = sprintf("SELECT rents.id, rents.begin, rents.end, IF(cars.price = 0.00, models.price, cars.price) as price FROM (rents INNER JOIN cars ON rents.car = cars.id) INNER JOIN models ON cars.model = models.id WHERE rents.id = '%s'",
-    $db->real_escape_string($rentId)
-  );
+  if (!$isCar) {
+    $query = sprintf("SELECT rents.id, rents.begin, rents.end, IF(cars.price = 0.00, models.price, cars.price) as price FROM (rents INNER JOIN cars ON rents.car = cars.id) INNER JOIN models ON cars.model = models.id WHERE rents.id = '%s'",
+      $db->real_escape_string($id)
+    );
+  }
+  else {
+    $query = sprintf("SELECT cars.id, IF(cars.price = 0.00, models.price, cars.price) as price FROM cars INNER JOIN models ON cars.model = models.id WHERE cars.id = '%s'",
+      $db->real_escape_string($id)
+    );
+  }
 
   $rents = $db->query($query);
   if ($rents->num_rows == 0) {
@@ -13,7 +20,23 @@ function rent_price( $rentId ) {
   }
   else {
     $rent = $rents->fetch_assoc();
-    $price = (float) (abs($rent['begin'] - $rent['end']) / 60 / 60 / 24 ) * (float) $rent['price'];
+
+    if (array_key_exists('begin', $rent) && array_key_exists('end', $rent)) {
+      $price = (float) (abs($rent['begin'] - $rent['end']) / 60 / 60 / 24 ) * (float) $rent['price'];
+    }
+    else if ($from !== null && $to !== null) {
+      list($from_year, $from_month, $from_day) = explode("-", $from);
+      $from = mktime(0, 0, 0, $from_month, $from_day, $from_year);
+
+      list($to_year, $to_month, $to_day) = explode("-", $to);
+      $to = mktime(23, 59, 59, $to_month, $to_day, $to_year);
+
+      $price = (float) (abs($from - $to) / 60 / 60 / 24 ) * (float) $rent['price'];
+    }
+    else {
+
+      $price = (float) $rent['price'];
+    }
 
     return str_replace('.', ',', number_format($price, 2));
   }
